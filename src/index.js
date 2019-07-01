d00ba1b6c795979704283b81ea4111fab1f26a8e
Samarth Keskar <samarthkeskar363@gmail.com>
2019-07-01 13:26:49 +0530

    signup component done validation :smiley_cat:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SignUpComponent from "./components/Sign-Up.js";
+import LoginComponent from "./components/Login.js";
 
 
 class Home extends React.Component {
   render() {
-    return ( 
-     <h1>hello world</h1>
-    );
+   
   }
 }
 
