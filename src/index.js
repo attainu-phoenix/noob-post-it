99e5c1755aa670d9264edbe0a17a2eb13ae8d54b
Samarth Keskar <samarthkeskar363@gmail.com>
2019-07-01 13:24:06 +0530

    signup component done validation :smiley_cat:

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
 import SignUpComponent from "./components/Sign-Up.js";
 
 
 class Home extends React.Component {
   render() {
     return ( 
-      <div className="container-fullwidth">
-      <div className="row">
-        <div className="col-md-12">
-          <SignUpComponent />
-        </div>
-      </div>
-      </div>
+     <h1>hello world</h1>
     );
   }
 }
